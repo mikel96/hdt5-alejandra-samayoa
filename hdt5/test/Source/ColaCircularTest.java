167f2b4d0840c796db51805bd6eb24cf7b1016e5
alejandra.samayoag <alejandra.samayoag@gmail.com>
2011-09-01 07:06:58 +0000

    Hoja de Trabajo 5 - faltan casos de prueba

---
+++ hdt5/test/Source/ColaCircularTest.java
@@ -0,0 +1,66 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Source;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Pingus
+ */
+public class ColaCircularTest {
+    
+    public ColaCircularTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of size method, of class ColaCircular.
+     */
+    @Test
+    public void testSize() {
+    }
+
+    /**
+     * Test of enqueue method, of class ColaCircular.
+     */
+    @Test
+    public void testEnqueue() {
+    }
+
+    /**
+     * Test of dequeue method, of class ColaCircular.
+     */
+    @Test
+    public void testDequeue() {
+    }
+
+    /**
+     * Test of peek method, of class ColaCircular.
+     */
+    @Test
+    public void testPeek() {
+    }
+}
